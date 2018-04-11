4f3a26ff48aa538d9b2643e59fae988edcfa201e
romasmi <romasmi@ro.ru>
2018-04-11 22:37:21 +0300

    Added autotes for empty search line

--- lab_1/task_1/test.bat
+++ lab_1/task_1/test.bat
@@ -9,7 +9,7 @@ rem Non existing input file
 IF NOT ERRORLEVEL 1 goto error
 
 rem Empty search string
-%PROGRAM% sonnete_17_by_shakespeare.txt %TEMP%\updated_file_copy.txt "123"
+%PROGRAM% sonnete_17_by_shakespeare.txt %TEMP%\updated_file_copy.txt ""
 IF NOT ERRORLEVEL 1 goto error
 
 rem Incorrect output file - current program
@@ -20,6 +20,12 @@ rem Correct input with one line
 %PROGRAM% sonnete_17_by_shakespeare_one_line.txt %TEMP%\updated_file_copy.txt "and" "enjunction"
 IF ERRORLEVEL 1 goto error
 fc.exe sonnete_17_by_shakespeare_one_line_updated.txt %TEMP%\updated_file_copy.txt
+IF ERRORLEVEL 1 goto error
+
+rem Empty search line 
+%PROGRAM% sonnete_17_by_shakespeare_one_line.txt %TEMP%\updated_file_copy.txt "" "enjunction"
+IF ERRORLEVEL 1 goto error
+fc.exe sonnete_17_by_shakespeare_one_line.txt %TEMP%\updated_file_copy.txt
 IF ERRORLEVEL 1 goto error
 
 rem Correct input with multiline input
