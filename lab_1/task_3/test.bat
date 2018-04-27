9c8007a4134e0c48c59b4d17a3a9c57d98508344
romasmi <romasmi@ro.ru>
2018-04-28 00:38:45 +0300

    Solution updated. Added test to lab_1/task_3

---
+++ lab_1/task_3/test.bat
@@ -0,0 +1,58 @@
+set PROGRAM="%~1"
+
+rem Empty input
+%PROGRAM%
+IF NOT ERRORLEVEL 1 goto error
+
+rem Empty file
+%PROGRAM% empty.txt
+IF NOT ERRORLEVEL 1 goto error
+
+rem Incorrect matrix
+%PROGRAM% incorrect_matrix.txt
+IF NOT ERRORLEVEL 1 goto error
+
+rem Irregular matrix
+%PROGRAM% irregular_matrix.txt
+IF NOT ERRORLEVEL 1 goto error
+goto good
+
+rem Non conformity of input radix and input number
+%PROGRAM% 10 11 Ff
+IF NOT ERRORLEVEL 1 goto error
+
+rem Input number overflow
+%PROGRAM% 10 16 1234567891145655645
+IF NOT ERRORLEVEL 1 goto error	
+
+rem Equal radix
+%PROGRAM% 10 10 10 > %TEMP%\output.txt
+IF ERRORLEVEL 1 goto error
+fc.exe 10_10_10.txt %TEMP%\output.txt
+IF ERRORLEVEL 1 goto error
+
+rem Convert zero
+%PROGRAM% 10 11 0 > %TEMP%\output.txt
+IF ERRORLEVEL 1 goto error
+fc.exe 10_11_0.txt %TEMP%\output.txt
+IF ERRORLEVEL 1 goto error
+
+rem Correct input
+%PROGRAM% 16 2 fF > %TEMP%\output.txt
+IF ERRORLEVEL 1 goto error
+fc.exe 16_2_255.txt %TEMP%\output.txt
+IF ERRORLEVEL 1 goto error
+
+rem Correct negaive input number
+%PROGRAM% 16 2 -Ff > %TEMP%\output.txt
+IF ERRORLEVEL 1 goto error
+fc.exe 16_2_minus_255.txt %TEMP%\output.txt
+IF ERRORLEVEL 1 goto error
+
+:good
+echo Program testing succeded
+exit 0
+
+:error
+echo Program testing failed
+exit 1
