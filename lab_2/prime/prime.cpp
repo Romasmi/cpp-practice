08fbb58d21c2a97f11343120f567b1c92130673b
romasmi <romasmi@ro.ru>
2019-03-14 11:40:31 +0300

    Optimize alghoritm

--- lab_2/prime/prime.cpp
+++ lab_2/prime/prime.cpp
@@ -14,29 +14,18 @@ PrimeSet GeneratePrimeNumbersSet(const unsigned int upperBound)
 {
 	PrimeSet primeSet;
 	vector<bool> primeVector(upperBound + 1);
-
 	fill(primeVector.begin(), primeVector.end(), true);
 
 	if (upperBound >= 2)
 	{
 		primeSet.insert(2);
 	}
-
 	for (unsigned int i = 3; i <= upperBound; i += 2)
 	{
-		if (!primeVector[i])
+		if (primeVector[i])
 		{
-			continue;
-		}
-		primeSet.insert(i);
-		for (unsigned int j = i * i; j <= upperBound; j += 2)
-		{
-			if (!primeVector[j])
-			{
-				continue;
-			} 
-
-			if (j % i == 0)
+			primeSet.insert(i);
+			for (unsigned int j = i * i; j <= upperBound; j += i)
 			{
 				primeVector[j] = false;
 			}
