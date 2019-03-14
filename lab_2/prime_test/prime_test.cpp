cd1bec6caff86bc7f6ba8ee6662b35b096649012
romasmi <romasmi@ro.ru>
2019-03-14 11:09:55 +0300

    Add test to check primes count

--- lab_2/prime_test/prime_test.cpp
+++ lab_2/prime_test/prime_test.cpp
@@ -12,13 +12,13 @@ TEST_CASE("Test primes generation")
 	REQUIRE(testSet == GeneratePrimeNumbersSet(0));
 
 	/*one prime*/
-	testSet = { 1 };
-	REQUIRE(testSet == GeneratePrimeNumbersSet(1));
+	testSet = { 2 };
+	REQUIRE(testSet == GeneratePrimeNumbersSet(2));
 
 	/*some primes*/
-	testSet = { 1, 2, 3, 5, 7, 11, 13 };
+	testSet = { 2, 3, 5, 7, 11, 13 };
 	REQUIRE(testSet == GeneratePrimeNumbersSet(13));
 
-	unsigned int maxSetSize = 5761455;
-	REQUIRE(maxSetSize == GeneratePrimeNumbersSet(100000000).size());
+	REQUIRE(500 == GeneratePrimeNumbersSet(3571).size());
+	REQUIRE(5761455 == GeneratePrimeNumbersSet(100000000).size());
 }
