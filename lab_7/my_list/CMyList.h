debc5bec450b3bf65cf3e9f91b72efb9e721c4b8
romasmi <romasmi@ro.ru>
2019-06-27 20:15:04 +0300

    Add my_list and tests

---
+++ lab_7/my_list/CMyList.h
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <memory>
+
+template<class T>
+class CMyList
+{
+public:
+	CMyList()
+	{
+		
+	}
+
+	~CMyList()
+	{
+
+	}
+
+private:
+	struct Node
+	{
+		T item;
+		std::unique_ptr prev;
+		std::unique_ptr next;
+	};
+};
